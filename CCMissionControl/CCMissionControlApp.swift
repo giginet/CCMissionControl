06e24fbf32ef4c41ced03adfeb58febd9f58773d
giginet <giginet.net@gmail.com>
2026-04-01 22:00:44 +0900

    Initial Commit

---
+++ CCMissionControl/CCMissionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCMissionControlApp.swift
+//  CCMissionControl
+//
+//  Created by giginet on 2026/04/01.
+//
+
+import SwiftUI
+
+@main
+struct CCMissionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCMissionControlTests/CCMissionControlTests.swift
@@ -0,0 +1,19 @@
+//
+//  CCMissionControlTests.swift
+//  CCMissionControlTests
+//
+//  Created by giginet on 2026/04/01.
+//
+
+import Testing
+@testable import CCMissionControl
+
+struct CCMissionControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
